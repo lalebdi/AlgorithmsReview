ae80e1c8f840bdf948879db6d4346b7c2f6a745f
lalebdi <loai.alebdi@gmail.com>
2021-03-22 22:12:42 -0400

    refactored the memoized fibonnaci

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -5,7 +5,7 @@ function fibonnaci(num){
     return (fibonnaci(num-1) + fibonnaci(num-2));
 }
 
-console.log(fibonnaci(1000))
+console.log(fibonnaci(5))
 
 // The above is O(1.6^n)
 
@@ -14,6 +14,15 @@ console.log(fibonnaci(1000))
 
 function fib(n, memo=[]){
     if(memo[n] !== undefined) return memo[n];
+    if(n <= 2) return 1;
+    var res = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = res;
+    return res;
+}
+
+console.log(fib(5))
+
+function fib2(n, memo=[undefined, 1, 1]){
     if(n <= 2) return 1;
     var res = fib(n-1, memo) + fib(n-2, memo);
     memo[n] = res;
