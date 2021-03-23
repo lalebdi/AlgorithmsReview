f6def1d0c1ced32fe9332c5bd9140246abadbb4c
lalebdi <loai.alebdi@gmail.com>
2021-03-22 22:07:37 -0400

    memoization

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -5,4 +5,18 @@ function fibonnaci(num){
     return (fibonnaci(num-1) + fibonnaci(num-2));
 }
 
-console.log(fibonnaci(5))
+console.log(fibonnaci(1000))
+
+// The above is O(1.6^n)
+
+// Below is more optimal using memoization:
+
+
+function fib(n, memo=[]){
+    if(memo[n] !== undefined) return memo[n];
+    if(n <= 2) return 1;
+    var res = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = res;
+    return res;
+}
+
